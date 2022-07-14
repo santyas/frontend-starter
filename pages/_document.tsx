046d2020a940d5cc1edb2e2d3796318060cd2dc7
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2022-07-14 14:00:15 -0300

    chore: update gtm tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,31 +23,23 @@ class MyDocument extends Document {
 					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
 					<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet"/>
 
-					{/*
-					<Script
-						src="https://www.googletagmanager.com/gtag/js?id=IIIIIIDDDDDD"
-						strategy="afterInteractive"
-					/>
-					<Script id="google-analytics" strategy="afterInteractive">
+					{/*<Script id="google-analytics" strategy="afterInteractive">
 						{`
-						window.dataLayer = window.dataLayer || [];
-						function gtag(){window.dataLayer.push(arguments);}
-						gtag('js', new Date());
-						
-						gtag('config', 'GA_MEASUREMENT_ID');
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-WXCHRF9');
 					`}
-					</Script>
-					*/}
+					</Script>*/}
 
 				</Head>
 				<body>
-				{/*
-				<Script id="google-analytics" strategy="afterInteractive">
+				{/*<Script id="google-analytics" strategy="afterInteractive">
 					{`
-						<iframe src="https://www.googletagmanager.com/ns.html?id=IIIIIDDDDD" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+						<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXCHRF9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 					`}
-				</Script>
-				*/}
+				</Script>*/}
 
 				<Main/>
 				<NextScript/>

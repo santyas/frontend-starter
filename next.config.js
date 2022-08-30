934fa39da8806271a48de9f2018c2c6c773dd03c
Santiago Nicolás Grufi <santiago.grufi@wearevernon.com>
2022-08-30 15:33:42 -0300

    chore: update sg token

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    SENDGRID_API_KEY: "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",
+    SENDGRID_API_KEY: "SG",
   },
 }
 
